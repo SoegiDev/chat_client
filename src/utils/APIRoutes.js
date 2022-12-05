f80458ee6ece76ae88a5fe15f14af830a509178c
Soegidev <soegidev.id@gmail.com>
2022-12-05 14:08:56 +0700

    init

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.VUE_APP_URL;
+export const host = "http://103.187.146.246:4000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
